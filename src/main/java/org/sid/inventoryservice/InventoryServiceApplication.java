fb85d31c7b2abc862e0f888940818ea79f6eeeae
TARHINE <tarhinereda@gmail.com>
2022-02-19 16:59:18 +0100

    First commit

---
+++ src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,55 @@
+package org.sid.inventoryservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+     @Bean
+     CommandLineRunner strat(ProductRepository productRepository){
+        return args -> {
+          productRepository.save(new Product(null,"Ordinateur",700,12));
+          productRepository.save(new Product(null,"Imprimante",600,120));
+          productRepository.save(new Product(null,"Smartphone",500,11));
+          productRepository.findAll().forEach(p -> {
+              System.out.println(p.getName());
+          });
+
+        };
+     }
+}
+    // je vais maittre tous dans la meme classe
+
+//1er chose
+@Entity // c'est une entite JPA : + @Id et @GeneratedValue
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+class Product{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private double quantity;
+}
+
+//2eme chose : creation d'une interface pour Spring Data
+@RepositoryRestResource
+interface ProductRepository extends JpaRepository<Product,Long>{
+
+}
